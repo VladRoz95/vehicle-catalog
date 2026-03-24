6c7649ab57c73c1b40f43657f25a58c8e099f2a0
Vlad Rozmarytsia <vladroz1995@gmail.com>
2026-03-24 18:09:00 +0200

    some change with route

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/vehicle-catalog">
+    <HashRouter basename="/vehicle-catalog">
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </HashRouter>
+  </StrictMode>,
 );
